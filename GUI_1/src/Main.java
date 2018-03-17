ef56ab0b5d4e041e4d94d821febdd232d2d68342
PatrykMobile <patrykdabrowski0@gmail.com>
2018-03-17 09:47:02 +0100

    Update

--- GUI_1/src/Main.java
+++ GUI_1/src/Main.java
@@ -8,6 +8,25 @@ public class Main {
         Reactangle r = new Reactangle(3,5);
         System.out.println();
         r.draw();
+
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Square(2);
+        shapes[1] = new Reactangle(2,5);
+
+        Square anon = new Square(10){
+            void draw(){
+                System.out.println("  .  ");
+                System.out.println(" .. ");
+                System.out.println("....");
+            }
+        };
+        shapes[2] = anon;
+
+
+        for(int i = 0; i<3; i++){
+            System.out.println("Drukowanie ");
+            shapes[i].draw();
+        }
     }
 
 }
---
+++ GUI_1/src/Trapezoid.java
@@ -0,0 +1,16 @@
+public class Trapezoid extends Shape {
+    int base_top;
+    int base_bottom;
+    int height;
+
+    public Trapezoid(int base_top,int base_bottom, int height){
+        this.base_top = base_top;
+        this.base_bottom = base_bottom;
+        this.height = height;
+    }
+
+    @Override
+    void draw() {
+
+    }
+}
